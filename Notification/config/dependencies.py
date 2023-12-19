c75c35b7eb66bdbca9c1e305e2b19b57276c251f
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:01:28 +0330

    chore: a simple temporary permisson to restrict endpoind to internal services.

--- Notification/config/dependencies.py
+++ Notification/config/dependencies.py
@@ -1,3 +1,16 @@
+# In the name of GOD
+from fastapi import Header, HTTPException, Request
+from typing import Annotated, List
+
+from services import SERVICE_CLASSES
+
+
+async def get_service(request: Request):
+    if not request.headers.get("internal-service"):
+        raise HTTPException(detail="Access denied.", status_code=403)
+    return True
+
+
 class AllowedServices:
     def __init__(self, allowed_services:List):
         self.allowed_services = allowed_services
